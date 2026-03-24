420ee78b7f6cf75c8acbac79d5a473e2d0572208
Arslan Ibragimov <a.z.ibragimov@tbank.ru>
2026-03-24 17:30:28 +0500

    Add sqlite

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose()
 
-let nextId = 4;
-const users = [
-    {id: 1, name: 'Дюха'},
-    {id: 2, name: 'Яна'},
-    {id: 3, name: 'Арсланчик'}
-];
+
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.json({
-        items: users
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
     });
 });
 
 router.post('/', function (req, res, next) {
-    const newUser = {
-        id: nextId,
-        name: req.body.name,
-    }
-    nextId++;
-    users.push(newUser);
-    res.status(201).json(newUser);
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [req.body.name], (err) => {
+        if (err !== null) {
+            res.status(400).json({error: "Error during user creation"});
+        }
+    });
+    res.status(201);
 });
 
 router.get('/:id', function (req, res, next) {
