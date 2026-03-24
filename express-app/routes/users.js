3f83e6e46290422ca091d1550bd12238f4715b6b
Govoruhina <yana@govoruhina.com>
2026-03-24 17:19:13 +0500

    fix2

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+  { id: 1, name: 'Дюха' },
+  { id: 2, name: 'Яна' },
+  { id: 3, name: 'Арсланчик' }
+];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-    items: [
-      { id: 1, name: 'Андрей' },
-      { id: 2, name: 'Яна' },
-      { id: 3, name: 'Дмитрий' }
-    ]
+    items: users
   });
+});
+
+router.get('/:id', function(req, res, next) {
+  const id = parseInt(req.params.id, 10);
+  const user = users.find(u => u.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
